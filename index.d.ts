eba9bf41148ae3d40e4418b51ff080ba22df5a1b
Amin NAIRI <nairi.amin@gmail.com>
2019-01-26 18:02:23 +0100

    :memo: added usage example from within VSCode intellisense

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,11 @@
+/**
+ * @example
+ * del({ targets: ['dist/*'] })
+ * del({ targets: ['dist/*'], verbose: true })
+ * del({ targets: ['../dist/*'], force: true })
+ * del({ targets: ['dist/*'], dryRun: true })
+ * del({ targets: ['dist/*'], concurrency: 4 })
+ */
 export default function({ targets, verbose, ...rest }: {
   targets: any[],
   verbose?: boolean,
