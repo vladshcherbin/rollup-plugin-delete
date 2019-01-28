b5c9bf74b2ae8935f2f20d6396667d696eef3786
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2019-01-28 11:04:59 +0300

    Add babel and set node 8 as a target version

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,15 @@
+import babel from 'rollup-plugin-babel'
+
 export default {
   input: 'src/index.js',
   output: {
     file: 'dist/index.js',
     format: 'cjs'
   },
+  plugins: [
+    babel({
+      presets: [['@babel/preset-env', { targets: { node: 8 } }]]
+    })
+  ],
   external: ['del']
 }
