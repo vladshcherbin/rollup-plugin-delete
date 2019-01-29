4d7f440f1c0e3a83842354f44787108fc0253502
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2019-01-29 19:41:33 +0300

    Add ESM version

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,16 @@ import babel from 'rollup-plugin-babel'
 
 export default {
   input: 'src/index.js',
-  output: {
-    file: 'dist/index.js',
-    format: 'cjs'
-  },
+  output: [
+    {
+      file: 'dist/index.cjs.js',
+      format: 'cjs'
+    },
+    {
+      file: 'dist/index.esm.js',
+      format: 'esm'
+    }
+  ],
   plugins: [
     babel({
       presets: [['@babel/preset-env', { targets: { node: 8 } }]]
