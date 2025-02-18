8b72280cecd178082cba29f2b412510b979f3fbb
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2025-02-19 02:28:24 +0300

    Rewrite in TypeScript
    Resolve #18

---
+++ src/index.ts
@@ -0,0 +1,70 @@
+import { deleteAsync, type Options as DelOptions } from 'del'
+import type { AsyncPluginHooks, Plugin } from 'rollup'
+
+export interface Options extends DelOptions {
+  /**
+   * Rollup hook the plugin should use.
+   * @default 'buildStart'
+   * end
+   */
+  readonly hook?: AsyncPluginHooks
+
+  /**
+   * Delete items once. Useful in watch mode.
+   * @default false
+   */
+  readonly runOnce?: boolean
+
+  /**
+   * Patterns of files and folders to be deleted.
+   * @default []
+   */
+  readonly targets?: readonly string[] | string
+
+  /**
+   * Outputs removed files and folders to console.
+   * @default false
+   */
+  readonly verbose?: boolean
+}
+
+export default function del(options: Options = {}): Plugin {
+  const {
+    hook = 'buildStart',
+    runOnce = false,
+    targets = [],
+    verbose = false,
+    ...delOptions
+  } = options
+
+  let deleted = false
+
+  return {
+    name: 'delete',
+    // eslint-disable-next-line perfectionist/sort-objects
+    [hook]: async () => {
+      if (runOnce && deleted) {
+        return
+      }
+
+      const paths = await deleteAsync(targets, delOptions)
+
+      if (verbose || delOptions.dryRun) {
+        const message = delOptions.dryRun
+          ? `Expected files and folders to be deleted: ${paths.length}`
+          : `Deleted files and folders: ${paths.length}`
+
+        console.log(message)
+
+        if (paths.length) {
+          paths.forEach((path) => {
+            console.log(path)
+          })
+        }
+      }
+
+      // eslint-disable-next-line require-atomic-updates
+      deleted = true
+    }
+  }
+}
